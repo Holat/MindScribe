e566354e9935814e2703804d390384e5a3f6546d
oluwatobi <oluwatobiajayi001@gmail.com>
2025-01-26 15:27:17 +0100

    Testing 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="text-6xl text-red-500">Hello World!</div>
+      <div className="text-6xl">Hello World!</div>
     </>
   );
 }
